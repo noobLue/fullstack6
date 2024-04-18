aa21dbf69b6fcbbd0c92b7ef6616d1532cdecf54
nooblue <null>
2024-04-18 07:11:02 +0300

    6.14 anecdotes are fetched from json-server

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -2,8 +2,20 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdoteService'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll().then(d => {
+      dispatch(setAnecdotes(d))
+    })
+  }, [])
+
   return (
     <div>
       <Notification />
